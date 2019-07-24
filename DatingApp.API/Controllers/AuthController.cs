db8de7051cbb98b72b387fdbc69715b1fd45d7f8
Shyam Patrick <patrick.shyamkumar@gmail.com>
2019-07-25 01:44:07 +0800

    Token Validation + Validation + Authentication

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Text;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DatingApp.API.Controllers
 {
@@ -10,17 +16,43 @@ namespace DatingApp.API.Controllers
   [ApiController]
   public class AuthController : ControllerBase
   {
+    /*
+ 
+          o                        o       o         
+         <|>                      <|>     <|>        
+         / \                      < >     / >        
+       o/   \o        o       o    |      \o__ __o   
+      <|__ __|>      <|>     <|>   o__/_   |     v\  
+      /       \      < >     < >   |      / \     <\ 
+    o/         \o     |       |    |      \o/     o/ 
+   /v           v\    o       o    o       |     <|  
+  />             <\   <\__ __/>    <\__   / \    / \ 
+                                                     
+                                                     
+                                                     
+ 
+*/
     private readonly IAuthRepository _repo;
-    public AuthController(IAuthRepository repo)
+    private readonly IConfiguration _config;
+
+    public AuthController(IAuthRepository repo, IConfiguration config)
     {
       _repo = repo;
+      _config = config;
     }
 
+    /*
+ 
+                      
+          .   .       
+ .-..-,.-...--|-.-,.-.
+ '  `'-`-|'-' '-`'-'  
+       `-'            
+ 
+*/
     [HttpPost("register")]
-    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto) // [FromBody] ! [ApiController]
     {
-      // TODO Validate
-
       userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
       if (await _repo.UserExists(userForRegisterDto.Username))
       {
@@ -39,6 +71,48 @@ namespace DatingApp.API.Controllers
 
     }
 
+    /*
 
+
+     .       .   
+     | .-..-...-.
+     '-`-'`-|'' '
+          `-'    
+
+    */
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+      var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+      if (userFromRepo == null)
+        return Unauthorized();
+
+      var claims = new[]
+      {
+        new Claim(ClaimTypes.NameIdentifier, userFromRepo.id.ToString()),
+        new Claim(ClaimTypes.Name, userFromRepo.Username)
+      };
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return Ok(new
+      {
+        token = tokenHandler.WriteToken(token)
+      });
+    }
   }
 }
--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -1,6 +1,6 @@
 namespace DatingApp.API.Dtos
 {
-  public class UserForRegisterDto
+  public class UserForLoginDto
   {
     public string Username { get; set; }
     public string Password { get; set; }
