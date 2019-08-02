b75d38059d3a278143ec7f0667f627c53ccc6c59
Shyam Patrick <patrick.shyamkumar@gmail.com>
2019-08-02 18:53:00 +0800

    Error Handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
         console.log('Logged In Successfully');
       },
       error => {
-        console.log('failed to login');
+        console.log(error);
       }
     );
   }
