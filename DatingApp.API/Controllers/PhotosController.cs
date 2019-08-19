6d3ffac3436f63be5a63a5ab3a889ed39ce4af57
Shyam <patrick.shyamkumar@gmail.com>
2019-08-19 10:06:53 +0800

    more repairs to gitignore

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -1,7 +1,29 @@
+using AutoMapper;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
 namespace DatingApp.API.Controllers
 {
-    public class PhotosController
+  [Authorize]
+  [Route("api/users/{userId}/photos")]
+  [ApiController]
+  public class PhotosController : ControllerBase
+  {
+    private readonly IDatingRepository _repo;
+    private readonly IMapper _mapper;
+    private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+
+    public PhotosController(
+        IDatingRepository repo,
+        IMapper mapper,
+        IOptions<CloudinarySettings> cloudinaryConfig
+    )
     {
-        
+      _repo = repo;
+      _mapper = mapper;
+      _cloudinaryConfig = cloudinaryConfig;
     }
+  }
 }
